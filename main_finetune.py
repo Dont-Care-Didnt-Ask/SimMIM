9f91d5998068e8d8186f7c2e7849b6fbb222f547
Mikhail Seleznyov <myyycroft@yandex-team.ru>
2022-05-28 15:34:02 +0300

    Add nirvana snapshots for checkpoints

--- main_finetune.py
+++ main_finetune.py
@@ -137,6 +137,13 @@ def main(config):
         train_one_epoch(config, model, criterion, data_loader_train, optimizer, epoch, mixup_fn, lr_scheduler)
         if dist.get_rank() == 0 and (epoch % config.SAVE_FREQ == 0 or epoch == (config.TRAIN.EPOCHS - 1)):
             save_checkpoint(config, epoch, model_without_ddp, max_accuracy, optimizer, lr_scheduler, logger)
+            try:
+                import nirvana_dl.snapshot as snap
+                snap.dump_snapshot()
+                print('Checkpoint saved to snapshots.')
+            except Exception:
+                print('Checkpoint NOT save to snapshots!')
+                pass
 
         acc1, acc5, loss = validate(config, data_loader_val, model)
         logger.info(f"Accuracy of the network on the {len(dataset_val)} test images: {acc1:.1f}%")
